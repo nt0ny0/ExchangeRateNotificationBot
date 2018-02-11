7f6a3d6e70a418381ecad726e30bd7e4716f3de7
nt0ny0 <ntny1986@gmail.com>
2018-02-11 03:49:42 +0300

    add configyration

---
+++ App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using  TelegramProviders.Domain;
+using Telegram.Autofac.DI;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var configuration = configurationBuilder.Build();
+            var token = configuration["token"];
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TelegramModule(token));
+            var container = builder.Build();
+            var bot = container.Resolve<IMessengerBot>();
+            bot.Start().Wait();
+        }
+    }
+}
