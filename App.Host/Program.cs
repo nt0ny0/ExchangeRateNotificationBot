a68e4c57b96abe5494afb7ee83fc4c74b76de9b3
nt0ny0 <ntny1986@gmail.com>
2018-02-18 23:27:26 +0300

    add host

---
+++ App.Host/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using App;
+
+namespace App.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var configurationBuilder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+                var configuration = configurationBuilder.Build();
+                var app = new Application();
+                Console.WriteLine("Application stared");
+                app.Run(configuration);
+                WaitStopCommand();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        static void WaitStopCommand()
+        {
+            bool isContinue = true;
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                isContinue = false;
+            };
+            Console.WriteLine("Press CTRL+C to exit");
+            while (isContinue) { }
+            Console.WriteLine("Application shutdown");
+        }
+    }
+}
